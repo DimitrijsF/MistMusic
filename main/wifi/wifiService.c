bcd0b243114a8901491452e2c9f282c599e96d81
Vandel <darkkneize@gmail.com>
2026-09-16 19:38:58 +0300

    nvs fix

--- main/wifi/wifiService.c
+++ main/wifi/wifiService.c
@@ -5,6 +5,7 @@
 #include "esp_wifi.h"
 #include "esp_event.h"
 #include "esp_netif.h"
+#include "nvs_flash.h"
 
 #include "wifiService.h"
 #include "wifiHttp.h"
@@ -18,6 +19,16 @@ void WifiService_Init(void)
 {
     if (s_initialized)
         return;
+
+    esp_err_t ret = nvs_flash_init();
+
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    ESP_ERROR_CHECK(ret);
 
     ESP_ERROR_CHECK(
         esp_netif_init()
